422d0e641b4819945ef426caae040b23de74ecbb
Sara <saferr.dev@gmail.com>
2026-06-08 18:20:43 -0300

    feat: correction

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Hub/tree/gh-pages",
+  base: "/Hub/",
   plugins: [react()],
 });
